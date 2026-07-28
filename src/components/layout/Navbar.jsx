24ea3708e803454bbb62357e6f13dd60c7821148
cynthia <njugunacynthia32@gmail.com>
2026-07-28 03:06:02 +0300

    make the landing page navbar responsive

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -5,23 +5,24 @@ function Navbar() {
   return (
     <nav className="navbar">
       <div className="logo">
-        EventSphere
+        🌸 <span>EventSphere</span>
       </div>
 
       <ul className="nav-links">
         <li><a href="#features">Features</a></li>
         <li><a href="#how-it-works">How It Works</a></li>
-        <li><a href="#about">About</a></li>
         <li><a href="#reviews">Reviews</a></li>
       </ul>
 
-      <div className="nav-buttons">
-        <Link to="/login">
-          <button className="login-btn">Log In</button>
+      <div className="nav-actions">
+        <Link to="/login" className="login-link">
+          Log In
         </Link>
 
         <Link to="/register">
-          <button className="signup-btn">Sign Up</button>
+          <button className="signup-btn">
+            Sign Up
+          </button>
         </Link>
       </div>
     </nav>
